86f68725bfdaac853c28af5fef89796975599373
Felipe Nunes Azambuja <felipenunesazambuja@gmail.com>
2024-09-06 22:59:37 -0300

    Create pt-br.php

---
+++ locale/pt-br.php
@@ -0,0 +1,22 @@
+
+<?php
+
+declare(strict_types=1);
+
+return [
+    'NOT_EMPTY' => '%s não pode estar vazio!',
+    'EMAIL_INVALID' => 'Formato de %s é inválido!',
+    'ENUM_INVALID' => '%s inválido!',
+    'INTEGER_INVALID' => '%s deve ser um número!',
+    'NUMERIC_INVALID' => '%s deve ser um número!',
+    'NUMERIC_INVALID_LENGTH' => '%s deve ser um número de %d dígitos!',
+    'NUMERIC_INVALID_MIN_LENGTH' => '%s deve ter no mínimo %d dígitos!',
+    'NUMERIC_INVALID_MAX_LENGTH' => '%s deve ter no máximo %d dígitos!',
+    'STRING_INVALID_LENGTH' => '%s deve ter %d caracteres!',
+    'STRING_INVALID_MIN_LENGTH' => '%s deve ter no mínimo %d caracteres!',
+    'STRING_INVALID_MAX_LENGTH' => '%s deve ter no máximo %d caracteres!',
+    'VALIDATE_ARRAY_CLASS_INVALID' => '%s inválido!',
+    'VALIDATE_ARRAY_CLASS_INDEX_INVALID' => '%s no índice %d é inválido: %s',
+    'VALIDATE_CLASS_INVALID' => '%s inválido!',
+    'VALIDATE_CLASS_INVALID_NESTED' => '%s inválido: %s',
+];
